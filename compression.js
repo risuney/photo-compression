1d241183b5781b8a0e9f761f4cc9d8a07f49748e
risuney <honey197692@gmail.com>
2021-01-09 20:47:45 +0900

    change

--- compression.js
+++ compression.js
@@ -1,50 +1,48 @@
-var captureForm = document.querySelector('#file'),
-  canvas = document.querySelector('canvas'),
-  ctx = canvas.getContext('2d');
+//読み込む画像を指定
+var file_image = document.querySelector('#file');
+var canvas = document.querySelector('canvas');
+var content = canvas.getContext('2d');
 canvas.width = canvas.height = 0;
 
-captureForm.addEventListener('change', function() {
-var file = this.files[0],
-    image = new Image(),
-    reader = new FileReader(),
-    reduceFlag = document.querySelector('input[name="reduceFlag"]:checked') ? +document.querySelector('input[name="reduceFlag"]:checked').value : 0; // 縮小方法の確認。念のため、どちらもチェックが入ってない場合のことも考えている
-    size = +document.querySelector('#imageSize' + reduceFlag).value;
-if (file.type.match(/image.*/)) {
-  reader.onloadend = function() {
-    image.onload = function() { // 画像が読み込めた時の処理
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      if(reduceFlag == 1){
-          var w = size, h = image.height * (size/image.width);
-      }else{
-          var w = image.width * (size/100), h = image.height * (size/100);
+//画像読み込み
+file_image.addEventListener('change', function() {
+  var file = this.files[0];
+  var image = new Image();
+  var reader = new FileReader();
+  var size = document.getElementById('imageSize').value;
+  if (file.type.match(/image.*/)) {
+    reader.onloadend = function() {
+      image.onload = function() {
+        //画像を読み込んだときの処理
+        content.clearRect(0, 0, canvas.width, canvas.height);
+        var w = size;
+        var h = image.height * (size/image.width);
+
+        //画像のサイズを表示
+        input_1.innerHTML = (image.width + '×' + image.height + '(px)');
+        input_2.innerHTML = (w + '×' + h + '(px)');
+        east.innerHTML = ('→');
+        const hidden = ('hidden');
+        document.getElementById('down').classList.add('is-active');
+        document.getElementById('ref').classList.add('is-active');
+        document.getElementById('hint1').classList.add(hidden);
+        document.getElementById('hint2').classList.add(hidden);
+        document.getElementById('input_1').classList.remove(hidden);
+        document.getElementById('input_2').classList.remove(hidden);
+        document.getElementById('east').classList.remove(hidden);
+
+        // 縮小画像を表示
+        canvas.width = w;
+        canvas.height = h;
+        content.drawImage(image, 0, 0, w, h);
+        var img = document.createElement('img');
+        img.src = canvas.toDataURL('image/jpeg');
+        document.querySelector('div.img').appendChild(img);
       }
-
-      input_1.innerHTML = (image.width + '×' + image.height + '(px)');
-      input_2.innerHTML = (w + '×' + h + '(px)');
-      east.innerHTML = ('→');
-      const hidden = ('hidden');
-
-      document.getElementById('down').classList.add('is-active');
-      document.getElementById('ref').classList.add('is-active');
-      document.getElementById('hint1').classList.add(hidden);
-      document.getElementById('hint2').classList.add(hidden);
-      document.getElementById('input_1').classList.remove(hidden);
-      document.getElementById('input_2').classList.remove(hidden);
-      document.getElementById('east').classList.remove(hidden);
-
-      canvas.width = w;
-      canvas.height = h;
-      ctx.drawImage(image, 0, 0, w, h);
-
-      // 縮小画像を表示
-      var img = document.createElement('img');
-      img.src = canvas.toDataURL('image/jpeg');
-      document.querySelector('div.img').appendChild(img);
+      image.src = reader.result;
     }
-    image.src = reader.result;
+    reader.readAsDataURL(file);
   }
-  reader.readAsDataURL(file);
-}
 }, false);
 
 function downloadCanvas() {
@@ -57,9 +55,14 @@ function downloadCanvas() {
   link.click()
 }
 
-function input_click() {
-  document.getElementById('file').click()
-};
+
+$('#button').on('click', function(){
+  $('#file').click()
+})
+
+// function input_click() {
+//   document.getElementById('file').click()
+// };
 
 
 //ファイル名取得
